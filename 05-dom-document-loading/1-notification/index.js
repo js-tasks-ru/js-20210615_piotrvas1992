1a0e6dd230eebcbbf6097b7a413bcd7cca587a96
Piotr Vasilyeu <piotr.vasilyeu@p97.com>
2021-07-06 18:25:45 +0300

    Create event listener for table and tooltip

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,57 @@
 export default class NotificationMessage {
+  static instanceComponent;
+  element;
+  timer;
+
+  constructor(message = '', {duration = 2000, type = 'success'} = {}) {
+    this.message = message;
+    this.duration = duration;
+    this.seconds = duration / 1000;
+    this.type = type;
+
+    this.render();
+  }
+
+  get template() {
+    return `
+      <div class="notification ${this.type}" style="--value:${this.seconds}s">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">${this.type}</div>
+          <div class="notification-body">
+            ${this.message}
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  render() {
+    const element = document.createElement('div');
+    element.innerHTML = this.template;
+    this.element = element.firstElementChild;
+  }
+
+  show(wrapper = document.body) {
+    if (NotificationMessage.instanceComponent) {
+      NotificationMessage.instanceComponent.remove();
+    }
+
+    wrapper.append(this.element);
+    this.timer = setTimeout(()=>{ this.remove(); }, this.duration);
+
+    NotificationMessage.instanceComponent = this;
+  }
+
+  remove() {
+    clearTimeout(this.timer);
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+    NotificationMessage.instanceComponent = null;
+  }
+
 
 }
--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,4 +1,70 @@
+
 class Tooltip {
+  text = '';
+  element;
+  static #instance = null;
+
+  constructor() {
+    if (!Tooltip.#instance) {
+      Tooltip.#instance = this;
+    }
+
+    return Tooltip.#instance;
+  }
+
+  get template() {
+    return `
+      <div class="tooltip">${this.text}</div>
+    `;
+  }
+
+  render() {
+    Tooltip.#instance.element = document.createElement('div');
+    Tooltip.#instance.element.innerHTML = Tooltip.#instance.template;
+    Tooltip.#instance.element.style.position = 'absolute';
+    document.body.append(Tooltip.#instance.element);
+  }
+
+  initialize() {
+    document.addEventListener('pointerover', this.pointerOverCallback);
+    document.addEventListener('pointerout', this.pointerOut);
+    document.addEventListener('pointermove', this.pointerMove);
+  }
+
+  pointerOverCallback(event) {
+    if (Tooltip.#instance.element) {
+      Tooltip.#instance.element.remove();
+    }
+
+    if (event.target.dataset.tooltip === 'foo') {
+      Tooltip.#instance.text = 'foo';
+      Tooltip.#instance.render();
+    }
+    else if (event.target.dataset.tooltip === 'bar-bar-bar') {
+      Tooltip.#instance.text = 'bar-bar-bar';
+      Tooltip.#instance.render();
+    }
+  }
+
+  pointerOut() {
+    if (Tooltip.#instance.element) {
+      Tooltip.#instance.element.remove();
+    }
+  }
+
+  pointerMove(event) {
+    if (event.target.dataset.tooltip === 'foo' || event.target.dataset.tooltip === 'bar-bar-bar') {
+      Tooltip.#instance.element.style.left = event.clientX + 15 + 'px';
+      Tooltip.#instance.element.style.top = event.clientY + 15 + 'px';
+    }
+  }
+
+  destroy() {
+    Tooltip.#instance.element.remove();
+    document.removeEventListener('pointerover', this.pointerOverCallback);
+    document.removeEventListener('pointerout', this.pointerOut);
+    document.addEventListener('pointermove', this.pointerMove);
+  }
 
 }
 
